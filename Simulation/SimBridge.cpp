d3000dd223f11adc3003fccebcf9b4b10619fd9d
zendkash <zendkash@users.noreply.github.com>
2014-09-30 16:12:43 -0700

    Completed implementation of SimBridge
    Needs to be tested on computer since it was written in the GitHub online editor

--- Simulation/SimBridge.cpp
+++ Simulation/SimBridge.cpp
@@ -1,4 +1,5 @@
 #include "SimBridge.h"
+#include <math.h>
 
 SimBridge::SimBridge()
 {
@@ -7,10 +8,44 @@ SimBridge::SimBridge()
     theta = 0;//position information
     lspeed = 0;
     rspeed = 0;//speed information
+    width = 12;
+    length = 1;
 }
 
 //the input time is the timestep to be taken
 void SimBridge::updatePos(int msec)
 {
+    double dl = lspeed*msec/1000;
+    double dr = rspeed*msec/1000;
+    
+    double rotradius = dl*width/(dr-dl);
+    double rottheta = dl/rotradius;
+    
+    double thetanew = theta+rottheta;
+    
+    x = x + ( rotradius + width / 2 ) * ( cos(thetanew) - cos(theta) );
+    y = y + ( rotradius + width / 2 ) * ( sin(thetanew) - sin(theta) );
+    
+    theta = newtheta;
+}
 
+
+void getpos(double &tbx, double &tby, double &tbtheta)
+{
+    tbx = x;
+    tby = y;
+    tbtheta = theta;
+}
+
+void getsize(double &tbwidth, double &tblength)
+{
+    tbwidth = width;
+    tblength = length;
+}
+
+//Function may have to be modified to have some sort of gradual change in speed rather than a step function changing speed
+void setspeeds(double newlspeed, double newrspeed)
+{
+    lspeed = newlspeed;
+    rspeed = newrspeed;
 }
